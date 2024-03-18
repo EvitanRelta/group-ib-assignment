f186a5f1bd148d5bfafcac7688c8ebe4943ce517
EvitanRelta <shauntanzongzhi@gmail.com>
2024-03-18 21:10:25 +0800

    Style settings

--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -1,42 +1,49 @@
-import React, { useContext } from 'react'
+import React, { Fragment, useContext } from 'react'
 import { SettingsContext } from '../contexts'
+import './SettingsPage.css'
 
 export const SettingsPage: React.FC = () => {
     const settingsState = useContext(SettingsContext)
     return (
-        <div>
-            <div>
-                <label>Notification count</label>
-                <input
-                    type='number'
-                    value={settingsState.maxCount}
-                    onChange={(e) => settingsState.setMaxCount(Number(e.target.value))}
-                />
+        <div className='settings-container'>
+            <div className='settings-box'>
+                <label className='settings-title'>Notification count</label>
+                <div className='settings-input'>
+                    <input
+                        className='settings-input-box'
+                        type='number'
+                        value={settingsState.maxCount}
+                        onChange={(e) => settingsState.setMaxCount(Number(e.target.value))}
+                    />
+                </div>
             </div>
-            <div>
-                <label>Notification position</label>
-                <div>
+            <div className='settings-box'>
+                <label className='settings-title'>Notification position</label>
+                <div className='settings-options'>
                     {[1, 2, 3, 4].map((i) => (
-                        <label key={i}>
+                        <Fragment key={i}>
+                            <label>Position {i}</label>
                             <input
                                 type='radio'
                                 value={i}
                                 checked={settingsState.position === i}
                                 onChange={() => settingsState.setPosition(i)}
                             />
-                            Position {i}
-                        </label>
+                        </Fragment>
                     ))}
                 </div>
             </div>
-            <div>
-                <label>Notification disappear time</label>
-                <input
-                    type='number'
-                    value={settingsState.disappearTime}
-                    onChange={(e) => settingsState.setDisappearTime(Number(e.target.value))}
-                />
-                <span>sec</span>
+            <div className='settings-box'>
+                <label className='settings-title'>Notification disappear time</label>
+                <div className='settings-input'>
+                    <input
+                        className='settings-input-box'
+                        type='number'
+                        value={settingsState.disappearTime}
+                        onChange={(e) => settingsState.setDisappearTime(Number(e.target.value))}
+                    />
+                    <label>sec</label>
+                </div>
             </div>
         </div>
     )
